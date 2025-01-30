133b78f04228a8727bf85bc29386bd1f6062ca3f
Jere Miettinen <jp-om@hotmail.com>
2025-01-30 08:15:11 +0100

    create default shader program

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 void error_callback(int error, const char* description);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+int create_default_shader_program();
 
 void error_callback(int error, const char* description){
     fprintf(stderr, "Error: %s\n", description);
@@ -19,6 +20,58 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+int create_default_shader_program(){
+    const char *vertexShaderSrc  = 
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main(){\n"
+        "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}\0";
+    const char *fragmentShaderSrc = 
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main(){\n"
+        "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\0";
+
+    unsigned int vertexShader, fragmentShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
+
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
+
+    int complileSuccess;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &complileSuccess);
+    if (!complileSuccess){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR compiling default vertex shader" << std::endl;
+    }
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &complileSuccess);
+    if (!complileSuccess){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR compiling default fragment shader" << std::endl;
+    }
+    
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &complileSuccess);
+    if (!complileSuccess){
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR compiling shader program" << std::endl;
+    }    
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
 }
 
 int main(void)
@@ -50,6 +103,21 @@ int main(void)
 
     glfwSwapInterval(1);
     glfwSetKeyCallback(window, key_callback);
+
+    float vertices[] = 
+      {-0.5f,-0.5f, 0.0f,
+        0.5f,-0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+ };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int defaultShader;
+    defaultShader = create_default_shader_program();
+    glUseProgram(defaultShader);
 
     while (!glfwWindowShouldClose(window))
     {
