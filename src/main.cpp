44ef73c359bb821757ac26a68db7540d7009d742
Jere Miettinen <jp-om@hotmail.com>
2025-02-03 19:49:25 +0100

    add glm to the project

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,12 @@ int main(void)
         1, 2, 3  // second triangle
     };
 
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
     rd.create_mesh(vertices, sizeof(vertices), indices, sizeof(indices));
     Image img = rd.load_image("C:\\Users\\jp-om\\Documents\\repos\\fpsgl\\resources\\textures\\container.png");
     rd.create_texture(img);
