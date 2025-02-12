2cc7178665aa8d2b30c3263b2200dda556b91e9a
Jere Miettinen <jp-om@hotmail.com>
2025-02-12 18:57:14 +0100

    map file reading structure

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ int main(void)
         1, 2, 3  // second triangle
     };
 
-    parse_map("C:\\Users\\jp-om\\Documents\\repos\\fpsgl\\resources\\test.map");
+    parse_map("C:\\Users\\jp-om\\Documents\\repos\\fpsgl\\resources\\level1.map");
 
     Mesh mesh;
 
--- src/map_parser.cpp
+++ src/map_parser.cpp
@@ -6,9 +6,44 @@ void parse_map(const char* filePath)
     std::string mapContent;
     std::ifstream mapFile(filePath);
 
+    bool entityStart = false;
+    bool brushStart = false;
+    bool worldspawn_entity = true;
+    std::string prevLine = "";
+
     if (mapFile.is_open()){
         while (getline (mapFile,line)){
-            std::cout << line << std::endl;
+            if (entityStart){
+                if (worldspawn_entity){
+                    if (line.find("mapversion") != std::string::npos){
+                        if (line.find("220") == std::string::npos){
+                            std::cout << "ERROR: Map version 220 not used" << std::endl;
+                        }
+                    }
+                    if (line == "}" && prevLine == "}"){
+                        worldspawn_entity = false;
+                    }
+
+                    if (brushStart){
+                        if (line == "}"){
+                            brushStart = false;
+                        }
+                        if (line != "}" && line != "{"){
+                            std::cout << line << std::endl;
+                        }
+                    }
+                    if (line.find("brush") != std::string::npos){
+                        brushStart = true;
+                    }
+                    prevLine = line;
+                }
+                else{
+                    // TODO parse other entities
+                }
+            }
+            if (line.find("entity") != std::string::npos){
+                entityStart = true;
+            }
         }
         mapFile.close();
     }
